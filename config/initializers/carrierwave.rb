4058b003b6d28b68b85ae11341958cbf7be92475
leungleungtest1 <leungleungtest1@gmail.com>
2015-01-01 14:26:41 +0800

    make it better

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,12 @@
 CarrierWave.configure do |config|
-  if Rails.env.staging? || Rails.env.production?
+
     config.storage = :fog
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV["GMAIL_USERNAME"],                        # required
-    :aws_secret_access_key  => ENV['GMAIL_PP'],                        # required
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                        # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],                        # required
   }
-  config.fog_directory  = 'leungleungmyflix'                          # required
+  config.fog_directory  = 'leungleungflix'                          # required
   config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
-  else
-    config.storage = :file
-    config.enable_processing = Rails.env.development?
-  end
+
 end
